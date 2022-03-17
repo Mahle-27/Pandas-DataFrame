c7974fc69d493c023ea18fafa9a5d9bba78146c0
Mahle-27 <93587932+Mahle-27@users.noreply.github.com>
2022-03-17 09:14:40 +0200

    Update Panda Dataframe (2).py

--- Panda Dataframe (2).py
+++ Panda Dataframe (2).py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
 # In[11]:
 
 
 import pandas as pd
 
-Chips =['Cheetos','Fritos','Lays', 'Doritos']
-Cooldrinks = ['Stoney','Sprite', 'Coke', 'Fanta']
-Chocolates = [' Sneakers', 'Kit-Kat', 'Lunchbar', 'PS']
-Pies =['Chicken Curry', 'Steak & Kidney', 'Burger pie', 'Pepper steak']
-Fruit =['Peach', 'Banana', 'Pineapple', 'Strawberry']
-Cupcakes =['Choc chip', 'Blueberry', 'Red velvet', 'Sponge cake']
-Veggies =['Brocolli', 'Onion', 'Spinach', 'Potato']
+Chips =['Cheetos','Fritos','Lays', 'Doritos', 'Pringles', 'Willards']
+Cooldrinks = ['Stoney','Sprite', 'Coke', 'Fanta', 'Jive', 'Creme soda']
+Chocolates = [' Sneakers', 'Kit-Kat', 'Lunchbar', 'PS', 'Lindt','Twix']
+Pies =['Chicken Curry', 'Steak & Kidney', 'Burger pie', 'Pepper Steak', 'Cheese Burger Pie', 'Apple Pie']
+Fruit =['Peach', 'Banana', 'Pineapple', 'Mango','Strawberry', 'Pear']
+Cupcakes =['Choc chip', 'Blueberry', 'Red velvet', 'Sponge Cake', 'Biscuit Cake', 'Pound Cake']
+Veggies =['Brocolli', 'Onion', 'Spinach', 'Potato', 'Carrot', 'Cabbage']
 
 df = pd.DataFrame(list(zip(Chips,Cooldrinks,Chocolates,Pies,Fruit,Cupcakes,Veggies)),
-                  columns=['Chips','Cooldrinks','Chocolates','Pies', 'Fruit', 'Cupcakes', 'Veggies'])
+                  columns=['Chips','Cooldrink,s','Chocolates','Pies', 'Fruit', 'Cupcakes', 'Veggies'])
 df
-
 
 # In[ ]:
 
